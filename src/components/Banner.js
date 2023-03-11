5b438d84436b1c26f1a6eff814223b8759d3d00d
Sarah Kwon <55962587+sarahkwon@users.noreply.github.com>
2023-03-11 05:17:00 -0800

    Redesign banner heading

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -16,18 +16,28 @@ const Banner = () => {
     <Box layerStyle='section' className='banner_bg-image' id='home'>
       <Center h='100%'>
         <SimpleGrid columns={1}>
+          <Heading color='white' fontWeight='medium' size={['sm', 'md']}>
+            <Highlight
+              query={['Hello! I\'m']}
+              styles={{ px: '2', py: '0', bg: `${bannerColor}`, borderRadius:'7px', color: '#121212' }}
+            >
+            Hello! I&apos;m
+            </Highlight>
+          </Heading>
           <SlideFade ref={ref} in={inView} offsetX='-30px'>
-            <Heading textStyle='h2' size='3xl' as='h2'>
-              <Highlight
-                query={['Sarah']}
-                styles={{ px: '1', py: '0', bg: `${bannerColor}` }}
-              >
-              Hi! I&apos;m Sarah Kwon
-              </Highlight>
+            <Heading variant='bannerMain' size={['3xl', '4xl']}>
+              Sarah Kwon,
             </Heading>
           </SlideFade>
           <SlideFade ref={ref} in={inView} offsetY='30px'>
-            <Heading textStyle='h4' size='xl' as='h4'>A front-end developer</Heading>
+            <Heading
+              variant='bannerSub'
+              size={['md', 'lg']}
+              as='h4'
+              textAlign='center'
+            >
+              a front-end developer
+            </Heading>
           </SlideFade>
         </SimpleGrid>
       </Center>
